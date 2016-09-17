a235a87b4512e8415730395d627e03a9045ab230
Louis Vichy <lgvichy@gmail.com>
2016-09-17 11:25:56 -0400

    Migrating toward the best UI plan

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -4,15 +4,31 @@ import CopyrightBar from '../../components/CopyrightBar';
 
 import Sequencer from '../../components/Sequencer';
 
+import Bumpkit from '../../components/Bumpkit';
+
 import './index.css';
 
 class Home extends Component {
 
   render() {
+
+    let clips = [];
+
+    const bumpkit = new Bumpkit({
+      tempo: 96,
+      loop: 64
+    })
+
+    for (let i = 0; i < 8; i++) {
+      clips[i] = bumpkit.createClip();
+
+      clips[i].pattern = [];
+    }
+
     return (
       <div className="Home">
-        <Sequencer />
-        <CopyrightBar/>
+        <Sequencer {...this.props} {...this.state}/>
+        <CopyrightBar />
       </div>
     );
   }
